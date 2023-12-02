3f224218a9df75da17d1cde8193a2f4aa4f928a9
Wetooa <103910243+Wetooa@users.noreply.github.com>
2023-12-02 14:59:04 +0800

    pulling

--- src/Entities/Plants/Cabbage.java
+++ src/Entities/Plants/Cabbage.java
@@ -5,8 +5,17 @@ import java.awt.Graphics2D;
 
 public class Cabbage extends Plant {
 
+  int attack_cooldown = 60;
+  int attack_ctr = 0;
+
   @Override
-  public void update() {}
+  public void update() {
+    ++attack_ctr;
+    if (attack_ctr >= attack_cooldown) {
+      attack_ctr = 0;
+      // handle attack here
+    }
+  }
 
   @Override
   public void paintComponent(Graphics2D g) {
