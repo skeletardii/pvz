e787ae9790c357a414838b40f9fa1dae1d13a0af
Wetooa <103910243+Wetooa@users.noreply.github.com>
2023-12-11 22:29:37 +0800

    [adrian] reset

--- src/Entities/Plants/Wallnut.java
+++ src/Entities/Plants/Wallnut.java
@@ -1,38 +1,44 @@
 package Entities.Plants;
 
+import Entities.Misc.Plant;
+import Main.Global;
 import java.awt.Graphics2D;
-import java.awt.Image;
-import javax.swing.ImageIcon;
 
 public class WallNut extends Plant {
 
-  public static final Image sprite = new ImageIcon("assets/plants/wallnut.png")
-    .getImage();
+  public WallNut(int row, int col) {
+    super(
+      row,
+      col,
+      50,
+      4000,
+      SeedPacketRechargeTime.VERY_SLOW.getValue(),
+      "sunflower",
+      364,
+      365,
+      1
+    );
+    anim_start[0] = 4;
+    anim_end[0] = 28;
+    setFrame(4);
+  }
+
+  public WallNut(int row, int col, int health, int sunCost, String spriteName) {
+    super(
+      row,
+      col,
+      sunCost,
+      health,
+      SeedPacketRechargeTime.VERY_SLOW.getValue(),
+      spriteName,
+      364,
+      365,
+      1
+    );
+  }
 
   public WallNut() {
     this(-1, -1);
-  }
-
-  public WallNut(int row, int col) {
-    super(
-      new PlantBuilder()
-        .setRow(row)
-        .setCol(col)
-        .setHealth(4000)
-        .setSunCost(50)
-        .setPacketCooldown(SeedPacketRechargeTime.VERY_SLOW.getValue())
-        .setSprite(sprite)
-        .setSpriteWidth(509)
-        .setSpriteHeight(496)
-    );
-    anim_start[0] = 0;
-    anim_end[0] = 16;
-    offsetOY = 20;
-    anim_speed = 2;
-  }
-
-  public WallNut(PlantBuilder pBuilder) {
-    super(pBuilder);
   }
 
   @Override
@@ -42,30 +48,8 @@ public class WallNut extends Plant {
 
   public static class TallNut extends WallNut {
 
-    private static final Image sprite = new ImageIcon(
-      "assets/plants/tallnut.png"
-    )
-      .getImage();
-
     public TallNut(int row, int col) {
-      super(
-        new PlantBuilder()
-          .setRow(row)
-          .setCol(col)
-          .setHealth(8000)
-          .setSunCost(50)
-          .setPacketCooldown(SeedPacketRechargeTime.VERY_SLOW.getValue())
-          .setSprite(sprite)
-          .setSpriteWidth(414)
-          .setSpriteHeight(584)
-      );
-      anim_start[0] = 20;
-      anim_end[0] = 36;
-      scale = 0.2;
-      shadowScale = 1.25;
-      shadowOffsetY = 15;
-      offsetOY = 10;
-      anim_speed = 2;
+      super(row, col, 8000, 125, "sunflower");
     }
 
     public TallNut() {
