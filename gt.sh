9e813deab08a253f6c8e1d9fd756a850c167dd54
Wetooa <103910243+Wetooa@users.noreply.github.com>
2023-12-11 16:40:36 +0800

    [adrian] started working on pvz again shesh

--- gt.sh
+++ gt.sh
@@ -3,3 +3,4 @@
 git add .
 git commit -m "[Adrian] default commit"
 git push
+git pull
--- src/Entities/Plants/Cabbage.java
+++ src/Entities/Plants/Cabbage.java
@@ -3,14 +3,17 @@ package Entities.Plants;
 import Entities.Misc.Plant;
 import java.awt.Graphics2D;
 import java.awt.Image;
-
 import javax.swing.ImageIcon;
 
 public class Cabbage extends Plant {
 
-  private static final Image sprite = new ImageIcon("assets/plants/sunflower.png").getImage();
+  private static final Image sprite = new ImageIcon(
+    "assets/plants/sunflower.png"
+  )
+    .getImage();
   int attack_cooldown = 60;
   int attack_ctr = 0;
+
   public Cabbage(
     int row,
     int col,
