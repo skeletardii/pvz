efc9ed067bd14664ba1a6485004bf78d49daef4c
Wetooa <103910243+Wetooa@users.noreply.github.com>
2023-12-15 15:39:37 +0800

    football

--- src/Entities/Zombies/FootballZombie.java
+++ src/Entities/Zombies/FootballZombie.java
@@ -14,7 +14,12 @@ public class FootballZombie extends Zombie {
         .setMovementSpeed(ZombieSpeed.FAST.getValue()).setSprite(new ImageIcon("assets/zombies/football.png").getImage())
               .setSpriteWidth(420).setSpriteHeight(323)
     );
-
+    animStart[0] = 22;
+    animEnd[0] = 51;
+    animStart[1] = 52;
+    animEnd[1] = 86;
+    offsetOX = 35;
+    offsetOY = 20;
   }
 
 
